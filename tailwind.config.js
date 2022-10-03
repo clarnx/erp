9f6f37a8e4aa66c78efbfbcc13dad7f1b934fb0c
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2022-10-03 12:31:25 +0800

    refactor: removed unused gray palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,18 +22,6 @@ module.exports = {
         tertiary: ["Open Sans", ...fontFamily.sans],
       },
       colors: {
-        gray: {
-          // Customize it on globals.css :root
-          50: withOpacityValue("--tw-color-gray-50"),
-          100: withOpacityValue("--tw-color-gray-100"),
-          200: withOpacityValue("--tw-color-gray-200"),
-          300: withOpacityValue("--tw-color-gray-300"),
-          400: withOpacityValue("--tw-color-gray-400"),
-          500: withOpacityValue("--tw-color-gray-500"),
-          600: withOpacityValue("--tw-color-gray-600"),
-          700: withOpacityValue("--tw-color-gray-700"),
-          800: withOpacityValue("--tw-color-gray-800"),
-        },
         primary: {
           50: withOpacityValue("--tw-color-primary-50"),
           100: withOpacityValue("--tw-color-primary-100"),
