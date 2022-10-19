080b037e5825ff4cee37e57f85549481bdd69cdf
Ghozi Fidaul <ghozi@digit-sense.com>
2022-10-19 13:14:39 +0700

    feat: added new firsttime sign in API (#32)
    * feat: added new firsttime sign in API
    
    * fix: change status code when resource not found
    
    * fix: fix more status code in firsttime Sign In API
    
    * fix: accountNumber name in first time sign in API

---
+++ src/pages/api/auth/first-time-sign-in/FirstTimeSignIn.tsx
@@ -0,0 +1,58 @@
+import { check, validationResult } from "express-validator";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { mongoConnect } from "@/utils/middleware";
+
+import User from "@/models/User";
+
+import initMiddleware from "./helper/init-middleware";
+import validateMiddleware from "./helper/validate-middleware";
+import { RequestData, ResponseData } from "./types";
+
+const validateBody = initMiddleware(
+  validateMiddleware(
+    [
+      check("securityQuestion").isLength({ min: 1 }).isString(),
+      check("securityAnswer").isLength({ min: 1 }).isString(),
+      check("password").isLength({ min: 6 }).isString(),
+      check("accountNumber").isLength({ min: 1 }).isString(),
+      check("email").isLength({ min: 6 }).isEmail(),
+    ],
+    validationResult
+  )
+);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  if (req.method === "PUT") {
+    await validateBody(req, res);
+    const userData = req.body as RequestData;
+
+    try {
+      await mongoConnect();
+
+      await User.updateOne(
+        {
+          accountNumber: userData.accountNumber,
+          email: userData.email,
+        },
+        {
+          securityQuestion: {
+            question: userData.securityQuestion,
+            answer: userData.securityAnswer,
+          },
+          password: userData.password,
+          status: true,
+        }
+      );
+
+      res.json({ message: "Success!" });
+    } catch (e) {
+      res.status(500).json({ message: "error: " + e });
+    }
+  } else {
+    res.status(404).json({ message: "Wrong method!" });
+  }
+}
---
+++ src/pages/api/auth/first-time-sign-in/types/index.ts
@@ -0,0 +1,11 @@
+export type ResponseData = {
+  message: string;
+};
+
+export type RequestData = {
+  securityQuestion: string;
+  securityAnswer: string;
+  password: string;
+  email: string;
+  accountNumber: string;
+};
